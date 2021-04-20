4d5fb0d040d4e85e0d573b3ba6a7a30c23d96003
RiMaDhiWJi <xd23746mad858797@126.com>
2021-04-20 17:43:24 +0800

    更新 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+/**
 public class HelloWorld {
     /**
 
@@ -6,7 +7,26 @@ public class HelloWorld {
     * @param args
 
     */
+/**
     public static void main(String[] args) {
     System.out.println("Hello World!");
+    }
+}
+*/
+
+public class Helloworld extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(6000);
+                System.out.print("Hello world! by J... \n");
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new Helloworld().start();
     }
 }
